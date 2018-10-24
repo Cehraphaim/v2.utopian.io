337cf2964ee1db0333b0f83b7b33b7f8349ed32e
Ícaro Harry <me@icaro.io>
2018-10-24 14:19:16 -0200

    Add missing files to auth project

---
+++ packages/auth/src/pages/redirect/redirect.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  preFetch ({ store, redirect }) {
+    if (store.state.auth.user) {
+      window.location = process.env.UTOPIAN_DOMAIN
+    }
+  },
+  name: 'u-page-redirect',
+  mounted () {
+    if (this.$route.query.redirectUrl) {
+      window.location = this.$route.query.redirectUrl
+    } else {
+      this.$router.push('login')
+    }
+  }
+}
+</script>
+
+<template src="./redirect.pug" lang="pug"></template>
+
+<style src="./redirect.styl" lang="stylus"></style>
