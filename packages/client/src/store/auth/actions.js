88e6a3a973766e357f66132df6c55921f1c6ae61
Ícaro Harry <me@icaro.io>
2018-10-24 13:34:32 -0200

    Add b2a on client package and move startLogin functions to auth store

--- packages/client/src/store/auth/actions.js
+++ packages/client/src/store/auth/actions.js
@@ -1,5 +1,6 @@
 import { Cookies } from 'quasar'
 import API from 'src/plugins/api'
+import { btoa } from 'b2a'
 
 export const me = async (context) => {
   const payload = await API.call({
@@ -16,4 +17,17 @@ export const logout = ({ dispatch, commit }) => {
   Cookies.remove('access_token')
   Cookies.remove('refresh_token')
   commit('clear')
+}
+
+export const startGithubLogin = () => {
+  const stateParameter = btoa(`githublogin::${window.location.href}`)
+  window.location = `https://github.com/login/oauth/authorize?scope=read:user,repo&client_id=${process.env.GITHUB_CLIENT_ID}&state=${stateParameter}`
+}
+
+export const startSteemConnectLogin = () => {
+  let callbackURL = ''
+  if (typeof window !== 'undefined') {
+    callbackURL = `${window.location.protocol}//${window.location.host}`
+  }
+  window.location = `https://steemconnect.com/oauth2/authorize?client_id=${process.env.STEEMCONNECT_CLIENT_ID}&redirect_uri=${callbackURL}&response_type=code&scope=offline,comment,vote,comment_options,custom_json&state=steemconnectlogin`
 }
